388cbdc7df66e45e6a7402d03ac6515ae85371f4
Bob Dekonink <bdekonin@f0r5s9.codam.nl>
2021-09-07 12:02:52 +0200

    d08/ex01

--- d08/ex01/main.cpp
+++ d08/ex01/main.cpp
@@ -6,13 +6,29 @@
 /*   By: bdekonin <bdekonin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/04 12:23:39 by bdekonin      #+#    #+#                 */
-/*   Updated: 2021/09/04 12:23:51 by bdekonin      ########   odam.nl         */
+/*   Updated: 2021/09/07 12:01:55 by bdekonin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "span.hpp"
+#include <vector>
+#include <list>
+
+#define MAX 12
 
 int main()
 {
-	
+    std::srand(unsigned(std::time(nullptr)));
+    std::vector<int> v(MAX);
+    std::generate(v.begin(), v.end(), std::rand);
+
+    Span span(MAX);
+    Span spany(MAX);
+    
+    span.addNumber(v.begin(), v.end());
+    spany.addNumber(v);
+
+    span.print();
+    spany.print();
+    
 }
--- d08/ex01/span.hpp
+++ d08/ex01/span.hpp
@@ -6,7 +6,7 @@
 /*   By: bdekonin <bdekonin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/03 14:37:51 by bdekonin      #+#    #+#                 */
-/*   Updated: 2021/09/04 12:28:14 by bdekonin      ########   odam.nl         */
+/*   Updated: 2021/09/07 12:02:40 by bdekonin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,44 +17,119 @@
 # include <iostream>
 # include <exception>
 
-class span
+// 3
+// 8
+// 1
+// 2
+// 3
+class Span
 {
 	public:
 		/* Constructor  */
-		span(int n)
+		Span(int n)
 		: _n(n)
 		{
 		}
 
 		/* Destructor */
-		~span()
+		~Span()
 		{
 		}
 
 		/* Copy constructor */
-		span(const span &e);
+		Span(const Span &e);
 
 		/* Operation overload = */
-		span &operator=(const span &e);
+		Span &operator=(const Span &e);
+
+		/* Exceptions */
+		class ShortSpanException : public std::exception
+		{
+			const char *what() const throw()
+			{
+				return "ShortSpanException";
+			}
+		};
+		class LongSpanException : public std::exception
+		{
+			const char *what() const throw()
+			{
+				return "LongSpanException";
+			}
+		};
+		class FullVector : public std::exception
+		{
+			const char *what() const throw()
+			{
+				return "Vector is Full";
+			}
+		};
 
 		// Methods
 		void addNumber(int n)
 		{
-			if (this->_numbers.size() >= (int)this->_n)
-				throw std::exception();
+			if (this->_numbers.size() >= (size_t)this->_n)
+				throw FullVector();
 			this->_numbers.push_back(n);
+		}
+		template<typename T>
+		void addNumber(T begin, T end)
+		{
+			if (this->_numbers.size() >= (size_t)this->_n)
+				throw FullVector();
+			
+			while (begin != end)
+			{
+				this->_numbers.push_back(*begin);
+				begin++;
+			}
+		}
+		template<typename T>
+		void addNumber(T &vec)
+		{
+			if (this->_numbers.size() >= (size_t)this->_n)
+				throw FullVector();
+			
+			for (typename T::iterator it = vec.begin(); it != vec.end(); it++)
+				this->_numbers.push_back(*it);
 		}
 		int shortestSpan(void)
 		{
-			int temp = 4;
+			size_t size = this->_numbers.size();
+			if (size <= 1)
+				throw ShortSpanException(); // change to diff
 
-			for (size_t i = 0; i < this->_numbers.size(); i++)
+			std::sort(this->_numbers.begin(), this->_numbers.end());
+
+			size_t temp = (size_t)this->_numbers[1] - (size_t)this->_numbers[0];
+			
+			for (size_t i = 0; i < size; i++)
 			{
-				min_element(this->_numbers.begin(), this->_numbers.end());
+				if (temp > (size_t)this->_numbers[i + 1] - (size_t)this->_numbers[i])
+					temp = (size_t)this->_numbers[i + 1] - (size_t)this->_numbers[i];
 			}
 			return temp;
 		}
-	private:
+		int longestSpan(void)
+		{
+			size_t size = this->_numbers.size();
+			if (size <= 1)
+				throw LongSpanException(); // change to diff
+
+			std::vector<int>::iterator min;
+			std::vector<int>::iterator max;
+
+			min = min_element(this->_numbers.begin(), this->_numbers.end());
+			max = max_element(this->_numbers.begin(), this->_numbers.end());
+			
+			return *max - *min;
+		}
+		void print(void)
+		{
+			std::vector<int> v = this->_numbers;
+			for (int i = 0; i < (int)v.size(); i++)
+				std::cout << "[" << i << "] "<< v[i] << std::endl;
+		}
 		int	_n;
 		std::vector<int> _numbers;
 };
